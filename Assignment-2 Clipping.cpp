c81ad8b9cc0721b159620f1e625ed727b8ecc779
sppu1 <115922004+sppu1@users.noreply.github.com>
2022-12-29 03:38:09 +0530

    Update Assignment-2 Clipping.cpp

--- Assignment-2 Clipping.cpp
+++ Assignment-2 Clipping.cpp
@@ -1,127 +1,85 @@
+/*
+7. Write C++ program to implement Cohen Southerland line clipping algorithm.
+*/
 
-#include<iostream>
+
+#include<iostream.h>
+#include<conio.h>
 #include<graphics.h>
-#include <conio.h>
-
-using namespace std;
-
-
-class lineClip {
-	private :
-		int RIGHT = 2,LEFT = 1,TOP = 8,BOTTOM = 4;//1-0001,2-0010,4-0100,8-1000;
-		int x1,x2,y1,y2,x,y;
-		int xmin=200,ymin=200,xmax=400,ymax=400;
-	public : 
-		int getcode(int x,int y);
-		void clipping();
-};
-
-int lineClip :: getcode(int x,int y) {
-	int code = 0;
-	if(y>ymax)
-		code=TOP;
-	if(y<ymin)			
-		code=BOTTOM;
-	if(x<xmin)
-		code=LEFT;
-	if(x>xmax)
-		code=RIGHT;
-	return code;
+#include<math.h>
+void Window()
+{
+	line (200,200,350,200);
+	line(350,200,350,350);
+	line(200,200,200,350);
+	line(200,350,350,350);
 }
-
-
-void lineClip :: clipping() {
-int code1,code2;
-
-
-cout<<"Rectangle Window "<<endl;
-setcolor(15);
-rectangle(xmin,ymin,xmax,ymax);
-
-cout<<"\n Enter the line coordinate :";
-cout<<"\n Starting Coordinate :";// (100,300)
-cin>>x1>>y1;
-cout<<"\n Ending coordinate : ";//(500,300)
-cin>>x2>>y2;
-
-setcolor(15);
-line(x1,y1,x2,y2);
-getch();
-
-
-code1 = getcode(x1,y1);
-code2 = getcode(x2,y2);
-int temp;
-
-float m;
-int accept = 0;
-while(1) {
-	m = (float)(y2-y1)/(x2-x1);
-	
-	if(code1 == 0 && code2 ==0) 
+void Code(char c[4],float x,float y)
+{        c[0]=(x<200)?'1':'0';
+	 c[1]=(x>350)?'1':'0';
+	 c[2]=(y<200)?'1':'0';
+	 c[3]=(y>350)?'1':'0';
+}
+void Clipping  (char c[],char d[],float &x,float &y,float m)
+{
+	int flag=1,i=0;
+	for (i=0;i<4;i++)
 	{
-		accept = 1;
-		break;	
-	} 
-	else if((code1 & code2) != 0)
-	{
-		break;
-	} 
-	else {
-		if(code1 == 0)
-			temp = code2;
-		else
-			temp = code1;
-		
-		if(temp & TOP) {
-			x = x1 + (ymax-y1)/m;
-			y = ymax;
+		if(c[i]!='0' && d[i]!='0')
+		{
+			flag=0;
+			break;
 		}
-		else if(temp & BOTTOM) {
-			x = x1 + (ymin-y1)/m;
-			y = ymin;	
+		if(flag)
+		{
+			if(c[0]!='0')
+			{
+				y=m*(200-x)+y;
+				x=200;
+			}
+			else if(c[1]!='0')
+			{
+				y=m*(350-x)+y;
+				x=350;
+			}
+			else if(c[2]!='0')
+			{
+				x=((200-y)/m)+x;
+				y=200;
+			}
+			else if(c[3]!='0')
+			{
+				x=((350-y)/m)+x;
+				y=350;
+			}
 		}
-		else if(temp & LEFT){
-			y = y1 + m*(xmin-x1);
-			x = xmin; 	
-		}
-		else if(temp & RIGHT){
-			y = y1 + m*(xmax-x1);
-			x = xmax; 	
-		}
-		if(temp == code1){
-			x1 = x;
-			y1 = y;
-			code1 = getcode(x1,y2);  
-		}else {
-			x2 = x;
-			y2 = y;
-			code2 = getcode(x2,y2);
-		}
+		if (flag==0)
+			cout<<"Line lying outside";
 	}
 }
-
-
-if(accept == 1)
-	cleardevice();
-    rectangle(xmin,ymin,xmax,ymax);
-    setcolor(5);
-    line(x1,y1,x2,y2);
+void main()
+{
+int gdriver = DETECT, gmode, errorcode;
+float x1,y1,x2,y2;
+float m;
+char c[4],d[4];
+clrscr();
+initgraph(&gdriver, &gmode, "//Turboc3//bgi");
+cout<<"Enter coordinates";
+cin>>x1>>y1>>x2>>y2;
+cout<<"Before clipping";
+Window();
+line(x1,y1,x2,y2);
+getch();
+cleardevice();
+m=float((y2-y1)/(x2-x1));
+Code(c,x1,y1);
+Code(d,x2,y2) ;
+Clipping(c,d,x1,y1,m);
+Clipping(d,c,x2,y2,m);
+cout<<"After Clipping";
+Window();
+line(x1,y1,x2,y2);
 getch();
 closegraph();
-
-
-
 }
-int main() 
-{
-int gd = DETECT,gm;
-initgraph(&gd,&gm,NULL);
-lineClip obj;
-obj.clipping();
-
-
-return 0;
-}
-
-  
